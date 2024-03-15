8d04873f2597ad3607a20ed1e48b7078cbcdbfa3
Anahita <anahita.amiri103@gmail.com>
2024-03-15 17:50:25 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    // Ensure that .jsx files are resolved as JavaScript modules
+    extensions: [".js", ".jsx"],
+  },
+  server: {
+    // Set the MIME type for JavaScript files
+    mimeTypes: {
+      // Ensure that .js files are served with the correct MIME type
+      "application/javascript": ["js", "mjs"],
+    },
+  },
+});
