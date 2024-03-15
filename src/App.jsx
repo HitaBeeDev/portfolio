3f48f4139b958daa11f9156ef2a62f6c63e65988
Anahita <anahita.amiri103@gmail.com>
2024-03-15 17:46:53 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
     <div>
       <h1>MY PORTFOLIO</h1>
       <p>MY PORTFOLIO</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus sequi
+        magnam saepe omnis voluptatibus. In deleniti quisquam, repellat soluta
+        iste totam nobis aliquam vitae, magni distinctio illum veritatis
+        molestias earum?
+      </p>
     </div>
   );
 }
