90491c9f98573224e402f12a13a43ebff9ca547c
Anahita <anahita.amiri103@gmail.com>
2024-03-21 22:00:35 +0300

    starting projects

--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -1,13 +1,13 @@
 function ProjectsSection() {
   return (
-    <div>
-      ProjectsSection ProjectsSection ProjectsSection ProjectsSection
-      ProjectsSection ProjectsSection ProjectsSection ProjectsSection
-      ProjectsSection ProjectsSection ProjectsSection ProjectsSection
-      ProjectsSection ProjectsSection ProjectsSection ProjectsSection
-      ProjectsSection ProjectsSection ProjectsSection ProjectsSection
-      ProjectsSection ProjectsSection ProjectsSection
-    </div>
+    <section
+      id="experience"
+      className="lg:grid lg:grid-cols-12 gap-10 mt-32 lg:mt-40"
+    >
+      <div className="lg:col-span-2 bg-color1"></div>
+
+      <div className="lg:p-10 lg:pb-16 lg:pr-16 lg:col-span-10 p-3 border-r border-b lg:h-[25rem] h-[33rem] border-color3"></div>
+    </section>
   );
 }
 
