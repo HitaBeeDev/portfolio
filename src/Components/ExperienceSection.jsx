f982133baaad8bbde06d178ef75c8cd475ff7792
Anahita <anahita.amiri103@gmail.com>
2024-03-21 22:13:50 +0300

    border

--- src/Components/ExperienceSection.jsx
+++ src/Components/ExperienceSection.jsx
@@ -13,7 +13,7 @@ function AboutSection() {
     >
       <div className="lg:col-span-2 bg-color1"></div>
 
-      <div className="lg:p-10 lg:pb-16 lg:pr-16 lg:col-span-10 p-3 border-r border-b lg:h-[25rem] h-[33rem] border-color3">
+      <div className="lg:p-10 lg:pb-16 lg:pr-16 lg:col-span-10 p-3 border-r border-b lg:h-[25rem] h-[33rem] border-color2">
         <p className="lg:text-4xl text-xl text-color3 font-semibold tracking-widest">
           <span className="text-color4 font-medium lg:text-2xl text-lg">
             02.{" "}
