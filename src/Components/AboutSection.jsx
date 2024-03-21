0d2eda05175780436d3c23bafb4475d9b648e1ee
Anahita <anahita.amiri103@gmail.com>
2024-03-21 22:16:05 +0300

    border

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -3,8 +3,11 @@ import aboutMeImg from "../assets/aboutMeImg.png";
 
 function AboutSection() {
   return (
-    <section id="about" className="lg:grid lg:grid-cols-12 gap-10">
-      <div className="lg:col-span-9 lg:p-10 p-5 border-l border-t border-color2">
+    <section
+      id="about"
+      className="lg:grid lg:grid-cols-12 gap-10 lg:p-10 p-5 border-r border-color2"
+    >
+      <div className="lg:col-span-9 ">
         <p className="lg:text-4xl text-xl text-color3 font-semibold tracking-widest">
           <span className="text-color4 font-medium lg:text-2xl text-lg">
             01.{" "}
--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -4,7 +4,7 @@ function ProjectsSection() {
       id="projects"
       className="mt-32 lg:mt-40 lg:grid lg:grid-cols-12 gap-10"
     >
-      <div className="lg:col-span-9 lg:p-10 p-5 border-l border-t border-color2">
+      <div className="lg:col-span-9 lg:p-10 p-5 border-r border-color2">
         <p className="lg:text-4xl text-xl text-color3 font-semibold tracking-widest">
           <span className="text-color4 font-medium lg:text-2xl text-lg">
             02.{" "}
