76bb9db93c4df63600131a297165b6c1a7f28f44
Anahita <anahita.amiri103@gmail.com>
2024-03-21 22:16:59 +0300

    border

--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -2,9 +2,9 @@ function ProjectsSection() {
   return (
     <section
       id="projects"
-      className="mt-32 lg:mt-40 lg:grid lg:grid-cols-12 gap-10"
+      className="mt-32 lg:mt-40 lg:grid lg:grid-cols-12 gap-10 lg:p-10 p-5 border-l border-color2"
     >
-      <div className="lg:col-span-9 lg:p-10 p-5 border-r border-color2">
+      <div className="lg:col-span-9 ">
         <p className="lg:text-4xl text-xl text-color3 font-semibold tracking-widest">
           <span className="text-color4 font-medium lg:text-2xl text-lg">
             02.{" "}
