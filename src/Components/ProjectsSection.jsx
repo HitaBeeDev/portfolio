5d4861207e9e6caff1a69fa7f5fa8b3cfc0ed484
Anahita <anahita.amiri103@gmail.com>
2024-03-21 22:12:58 +0300

    ui

--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -1,12 +1,17 @@
 function ProjectsSection() {
   return (
     <section
-      id="experience"
-      className="lg:grid lg:grid-cols-12 gap-10 mt-32 lg:mt-40"
+      id="projects"
+      className="mt-32 lg:mt-40 lg:grid lg:grid-cols-12 gap-10"
     >
-      <div className="lg:col-span-2 bg-color1"></div>
-
-      <div className="lg:p-10 lg:pb-16 lg:pr-16 lg:col-span-10 p-3 border-l border-b border-color3"></div>
+      <div className="lg:col-span-9 lg:p-10 p-5 border-l border-t border-color3">
+        <p className="lg:text-4xl text-xl text-color3 font-semibold tracking-widest">
+          <span className="text-color4 font-medium lg:text-2xl text-lg">
+            02.{" "}
+          </span>{" "}
+          Some projects I've built:
+        </p>
+      </div>
     </section>
   );
 }
