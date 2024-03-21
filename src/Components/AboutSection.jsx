59937c9345debb948104b70a48e8bc23b760e55b
Anahita <anahita.amiri103@gmail.com>
2024-03-21 22:13:24 +0300

    border

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -4,7 +4,7 @@ import aboutMeImg from "../assets/aboutMeImg.png";
 function AboutSection() {
   return (
     <section id="about" className="lg:grid lg:grid-cols-12 gap-10">
-      <div className="lg:col-span-9 lg:p-10 p-5 border-l border-t border-color3">
+      <div className="lg:col-span-9 lg:p-10 p-5 border-l border-t border-color2">
         <p className="lg:text-4xl text-xl text-color3 font-semibold tracking-widest">
           <span className="text-color4 font-medium lg:text-2xl text-lg">
             01.{" "}
