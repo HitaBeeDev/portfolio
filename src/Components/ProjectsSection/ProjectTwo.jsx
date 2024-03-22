8766989412f5760b6f8c3eca4eeda6f93941fd4b
Anahita <anahita.amiri103@gmail.com>
2024-03-22 15:52:14 +0300

    project two

--- src/Components/ProjectsSection/ProjectTwo.jsx
+++ src/Components/ProjectsSection/ProjectTwo.jsx
@@ -5,9 +5,14 @@ import project1_Img2 from "../../assets/A-2.png";
 import project1_Img3 from "../../assets/A-3.png";
 import project1_Img4 from "../../assets/A-4.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowUpRightFromSquare,
+  faSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import projectTools from "./projectTools";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-function ProjectTwo() {
+function ProjectOne() {
   const images = [project1_Img1, project1_Img2, project1_Img3, project1_Img4];
 
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
@@ -17,55 +22,7 @@ function ProjectTwo() {
   };
 
   return (
-    <div className="lg:grid lg:grid-cols-12 mt-44 gap-10">
-      <div className="lg:col-span-5 flex justify-end items-center">
-        <div className="h-full p-3 flex flex-col justify-between items-end">
-          <p className="text-3xl font-semibold text-color4">NexStep</p>
-
-          <p className="text-sm text-right text-color5 bg-color2/30 p-5 rounded-[0.3rem]">
-            Enhanced React Productivity Dashboard combines a Habit Tracker,
-            To-Do List, and Pomodoro Timer into a single, streamlined
-            application. Designed for efficiency and ease of use, it's the
-            perfect tool for anyone looking to improve their productivity and
-            focus.
-          </p>
-
-          <ul className="flex flex-row text-color4 text-sm justify-center items-start gap-5 w-full flex-wrap">
-            <li className="border-color2 border rounded-[0.3rem] text-color3 text-xs p-2 h-6 flex items-center">
-              react
-            </li>
-
-            <li className="border-color2 border rounded-[0.3rem] text-color3 text-xs p-2 h-6 flex items-center">
-              react
-            </li>
-
-            <li className="border-color2 border rounded-[0.3rem] text-color3 text-xs p-2 h-6 flex items-center">
-              react
-            </li>
-
-            <li className="border-color2 border rounded-[0.3rem] text-color3 text-xs p-2 h-6 flex items-center">
-              react
-            </li>
-          </ul>
-
-          <div className="flex flex-row gap-3">
-            <a>
-              <FontAwesomeIcon
-                className="text-color4 w-3 h-3 hover:text-color4 transition-all duration-300"
-                icon={faSquare}
-              />
-            </a>
-
-            <a>
-              <FontAwesomeIcon
-                className="text-color4 w-3 h-3 hover:text-color4 transition-all duration-300"
-                icon={faSquare}
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-
+    <div className="lg:grid lg:grid-cols-12 mt-10 gap-10">
       <div className="lg:col-span-7 flex justify-center items-center">
         <div className="rounded-[0.3rem] w-full h-[20rem] relative overflow-hidden">
           <AnimatePresence initial={false} custom={selectedImageIndex}>
@@ -107,8 +64,57 @@ function ProjectTwo() {
           </div>
         </div>
       </div>
+
+      <div className="lg:col-span-5 flex justify-end items-center">
+        <div className="h-full p-3 flex flex-col justify-between items-end">
+          <p className="text-3xl font-semibold text-color4">NexStep</p>
+
+          <p className="text-sm text-right text-color5 bg-color2/30 p-5 rounded-[0.3rem]">
+            Enhanced React Productivity Dashboard combines a Habit Tracker,
+            To-Do List, and Pomodoro Timer into a single, streamlined
+            application. Designed for efficiency and ease of use, it's the
+            perfect tool for anyone looking to improve their productivity and
+            focus.
+          </p>
+
+          <ul className="flex flex-row text-color4 text-sm justify-end items-start gap-x-3 gap-y-2 w-full flex-wrap">
+            {projectTools.map((tool, index) => (
+              <li
+                key={index}
+                className="border-color2 border rounded-[0.3rem] text-color3 text-xs p-2 h-6 flex items-center"
+              >
+                {tool}
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex flex-row gap-3">
+            <a
+              href="https://github.com/HitaBeeDev/NexStep-Productivity-Dashboard"
+              target="_blank"
+              className="cursor-pointer"
+            >
+              <FontAwesomeIcon
+                className="text-color4 text-xl transition-all duration-500 hover:text-color5"
+                icon={faGithub}
+              />
+            </a>
+
+            <a
+              href="https://nexstep.vercel.app/"
+              target="_blank"
+              className="cursor-pointer"
+            >
+              <FontAwesomeIcon
+                className="text-color4 text-lg transition-all duration-500 hover:text-color5"
+                icon={faArrowUpRightFromSquare}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default ProjectTwo;
+export default ProjectOne;
