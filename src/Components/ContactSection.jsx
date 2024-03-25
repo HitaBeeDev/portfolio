bbfd894ca6630c1eb46b630919868dd39fa99b46
Anahita <anahita.amiri103@gmail.com>
2024-03-25 15:17:49 +0300

    contact

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -21,7 +21,7 @@ function ContactSection() {
 
       <div className="flex justify-center">
         <div className="bg-color3 lg:w-2/3 p-8 mt-10 flex flex-col gap-2 justify-center items-center bg-opacity-10 rounded-[0.3rem] shadow-xl backdrop-blur-sm backdrop-filter">
-          <p className="text-color4 text-sm font-normal tracking-widest	leading-loose text-center">
+          <p className="text-color5 text-sm font-thin tracking-widest	leading-loose text-center">
             If you're searching for a frontend developer to join your team, I'm
             here and ready to jump in! Feel free to reach out with any
             questions, proposals, or just to say hello! <br /> Looking forward
@@ -30,7 +30,10 @@ function ContactSection() {
 
           <button
             onClick={handleContactClick}
-            className="font-semibold text-color5 hover:bg-color1 text-xs mt-10 tracking-widest border border-color2 rounded-[0.3rem] p-3 transition-all duration-500"
+            className="text-sm font-semibold text-color4 transition-all duration-500
+             hover:text-color1 p-3 w-32 h-10 mt-8 flex justify-center items-center
+              border-2 rounded-[0.3rem] border-color3 
+               hover:border-color4 hover:bg-color4 shadow-xl"
           >
             Let's connect
           </button>
